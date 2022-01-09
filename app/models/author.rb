66f30377c41e2301133073ef6b699a8f6c8fcd2f
nickdossantos <nickdos32@gmail.com>
2022-01-09 10:25:36 -0500

    Author Profile &  CRUD

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,6 @@
 class Author < User
-  has_many :posts
-  has_many :comments
+  has_many :posts, foreign_key: :user_id, inverse_of: :author
+  has_many :comments, foreign_key: :user_id, inverse_of: :author
 
   def full_name
     "#{first_name} #{last_name}".strip
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  belongs_to :author, foreign_key: 'user_id', class_name: 'Author'
+  belongs_to :author, foreign_key: 'user_id', class_name: 'Author', inverse_of: :posts
   has_many :comments
 
   accepts_nested_attributes_for :comments
